44a0db7fa0b542bb913c3e4ce7ef5e89d0d522b5
iPwaoo <quentin.bouetel@gmail.com>
2024-03-13 15:15:20 +0100

    Mise à jour partie v3 et finale (je crois) commune

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr3/SmartQuiz/services/interfaces/IservicesQuestionnaire.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr3/SmartQuiz/services/interfaces/IservicesQuestionnaire.java
@@ -1,12 +1,13 @@
 package org.univ_paris8.iut.montreuil.qdev.tp2024.gr3.SmartQuiz.services.interfaces;
 
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr3.SmartQuiz.entities.bo.QuestionBO;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr3.SmartQuiz.entities.dto.QuestionnaireDTO;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr3.SmartQuiz.utiles.Exceptions.*;
 
 import java.util.List;
 
 public interface IservicesQuestionnaire {
 
-    public List<QuestionBO> chargerQuestionnaire(String addresseFichierCSV) throws NombreColoneIncorrect, FormatIncorrect;
+    public List<QuestionnaireDTO> chargerQuestionnaire(String addresseFichierCSV) throws NombreColoneIncorrect, FormatIncorrect, IdQuestionnaireNonInteger, IdQuestionNonInteger, LangueNonReconnue, NiveauNonReconnue, ReferenceNonUrl;
 
 }
